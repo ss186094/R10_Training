9f79f192b331af74b2c031a8e8024bff04b4ce64
ss186094 <ss186094@ncr.com>
2022-05-19 12:26:57 +0530

    Exercise 1-C#

--- Exercise1/Exercise1/Program.cs
+++ Exercise1/Exercise1/Program.cs
@@ -2,87 +2,154 @@
 
 public class Program
 {
+
     public static void Main()
     {
-        List<Employee> list= new List<Employee>();
-        Employee employee1=new Employee()
+        List<Employee> list = new List<Employee>();
+        start:
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1.Add an employee");
+        Console.WriteLine("2.Update an Employee");
+        Console.WriteLine("3.Search an Employee");
+        Console.WriteLine("4.Sort employees by name");
+        Console.WriteLine("5.Sort employees by salary");
+        Console.WriteLine("6.Delete an employee");
+        Console.WriteLine("7.Display all the employees");
+        int option=int.Parse(Console.ReadLine());
+       
+        string s = "-------------------------------------------------------------------";
+        switch (option)
         {
-            Id=1,
-            Name ="Paul",
-            Salary =5000,
-            Experience =3
+            case 1: add(list);
+                Console.WriteLine(s);
+                goto start;
+           
+                
+            case 2: Console.WriteLine("Enter the Id to be updated:");
+                    int id=int.Parse(Console.ReadLine());
+                    update(list,id);
+                    Console.WriteLine(s);
+                    goto start;
 
-        };
-        Employee employee2 = new Employee()
-        {
-            Id = 2,
-            Name = "Bob",
-            Salary = 6000,
-            Experience = 4
+            case 3: Console.WriteLine("Enter the employee Name:");
+                    String name=Console.ReadLine();
+                    Search(list, name);
+                    Console.WriteLine(s);
+                    goto start;
 
-        };
-        Employee employee3 = new Employee()
-        {
-            Id = 1,
-            Name = "Jack",
-            Salary = 3000,
-            Experience = 2
+            case 4: sortname(list);
+                    Console.WriteLine(s);
+                    goto start;
 
-        };
-        Employee employee4 = new Employee()
-        {
-            Id = 4,
-            Name = "Ross",
-            Salary = 8000,
-            Experience = 5
+            case 5: sortsalary(list);
+                    Console.WriteLine(s);
+                    goto start;
 
-        };
-        list.Add(employee1);
-        list.Add(employee2);
-        list.Add(employee3);
-        list.Add(employee4);
+            case 6: Console.WriteLine("Enter Id:");
+                    int id1 = int.Parse(Console.ReadLine());
+                    delete(list,id1);
+                    Console.WriteLine(s);
+                    goto start;
+
+            case 7:display(list);
+                    Console.WriteLine(s);
+                    goto start;
+            default:Console.WriteLine("Enter a valid option");
+                    Console.WriteLine(s);
+                    goto start;
+
+
+        }
         
-        //Updating Employee
-        foreach (Employee employee in list)
+
+
+
+    }
+
+
+
+
+
+    // add an employee
+    public static void add(List<Employee> list)
+    {
+        Console.WriteLine("Enter Id :");
+        int id=int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Name:");
+        String name=Console.ReadLine();
+        Console.WriteLine("Enter Salary:");
+        int sal=int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Experience");
+        int exp=int.Parse(Console.ReadLine());
+        Employee employee=new Employee();
+        employee.Id=id;
+        employee.Name=name;
+        employee.Salary=sal;
+        employee.Experience=exp;
+        list.Add(employee);
+    }
+
+
+
+    //Update an employee
+    public static void update(List<Employee> list,int id)
+    {   select:
+        Console.WriteLine("Choose an option");
+        Console.WriteLine("1.Name");
+        Console.WriteLine("2.Salary");
+        Console.WriteLine("3.Experience");
+        Console.WriteLine("4.End");
+        int option=int.Parse(Console.ReadLine());
+
+        foreach(Employee emp in list)
         {
-            if(employee.Name == "Jack")
+            if(emp.Id == id)
             {
-                employee.Salary = 4000;
+                switch (option)
+                {
+                    case 1: Console.WriteLine("Enter the updated name:");
+                        String update=Console.ReadLine();
+                        emp.Name=update;
+                        goto select;
+                    case 2: Console.WriteLine("Enter the updated salary");
+                        int updatesal=int.Parse(Console.ReadLine());
+                        emp.Salary=updatesal;goto select;
+                    case 3: Console.WriteLine("Enter the updated Experience:");
+                        int updateexp=int.Parse(Console.ReadLine());
+                        emp.Experience=updateexp;goto select;
+                    case 4:
+                        break ;
+                    default: Console.WriteLine("Enter correct value");
+                        goto select;
+                }
+                break;
             }
         }
+        Console.WriteLine("Updated Successfully");
+    }
+
+
+
+
+    // Searching an employee
+    public static void Search(List<Employee> list, String name)
+    {
         foreach (Employee employee in list)
         {
-            Console.WriteLine(employee.Name+"    "+employee.Salary);
-        }
-
-
-        //Getting Employee Details
-
-        foreach (Employee employee in list)
-        {
-            if (employee.Id == 2)
+            if (employee.Name == name)
             {
-                Console.WriteLine("Employee Name : {0}", employee.Name);
+                Console.WriteLine("Employee Id : {0}", employee.Id);
                 Console.WriteLine("Employee Salary : {0}", employee.Salary);
                 Console.WriteLine("Employee Experience : {0}", employee.Experience);
             }
         }
 
-        //Sorting Employee by Salary
-        Console.WriteLine("Before Sorting");
-        foreach (Employee employee in list)
-        {
-            Console.WriteLine(employee.Salary);
-        }
-        list.Sort();
-        Console.WriteLine("After Sorting");
-        foreach (Employee employee in list)
-        {
-            Console.WriteLine(employee.Salary);
-        }
+    }
 
-        //Sorting Employee by Name using Delegate
 
+    //Sorting Employee by Name using Delegate
+    public static void sortname(List<Employee> list)
+    {
         Console.WriteLine("Before Sorting");
         foreach (Employee employee in list)
         {
@@ -94,39 +161,58 @@ public class Program
         {
             Console.WriteLine(employee.Name);
         }
+    }
 
 
-        // Removing an employee
-        foreach(Employee employee in list)
+
+    //Sorting Employee by Salary
+    public static void sortsalary(List<Employee> list)
+    {
+        Console.WriteLine("Before Sorting");
+        foreach (Employee employee in list)
         {
-            if(employee.Id == 2)
+            Console.WriteLine(employee.Salary);
+        }
+        list.Sort();
+        Console.WriteLine("After Sorting");
+        foreach (Employee employee in list)
+        {
+            Console.WriteLine(employee.Salary);
+        }
+    }
+
+
+
+    //Delete an Employee
+    public static void delete(List<Employee> list,int id1)
+    {
+        foreach (Employee employee in list)
+        {
+            if (employee.Id == id1)
             {
                 list.Remove(employee);
                 break;
             }
-            
+
         }
-        foreach( Employee employee in list)
-        {
-            Console.WriteLine(employee.Name);
-        }
-
-        //Exception Handling
-
-        try
-        {
-           
-        }
-        catch(Exception ex)
-        {
-            Console.WriteLine("Employee Not Found");
-        }
-
-
-
+        
 
     }
+
+
+    // Display all the Employees
+    public static void display(List<Employee> list)
+    {
+        foreach (Employee emp in list)
+        {
+            Console.WriteLine("ID: {0} , Name: {1} , Salary: {2} , Experience: {3}",emp.Id,emp.Name,emp.Salary,emp.Experience);
+        }
+    }
+
 }
+
+
+
 
 public class Employee: IComparable<Employee>
 {
@@ -154,3 +240,107 @@ public class Employee: IComparable<Employee>
         //return this.Salary.CompareTo(other.Salary);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Employee employee1 = new Employee()
+//{
+//    Id = 1,
+//    Name = "Paul",
+//    Salary = 5000,
+//    Experience = 3
+
+//};
+//Employee employee2 = new Employee()
+//{
+//    Id = 2,
+//    Name = "Bob",
+//    Salary = 6000,
+//    Experience = 4
+
+//};
+//Employee employee3 = new Employee()
+//{
+//    Id = 1,
+//    Name = "Jack",
+//    Salary = 3000,
+//    Experience = 2
+
+//};
+//Employee employee4 = new Employee()
+//{
+//    Id = 4,
+//    Name = "Ross",
+//    Salary = 8000,
+//    Experience = 5
+
+//};
+//list.Add(employee1);
+//list.Add(employee2);
+//list.Add(employee3);
+//list.Add(employee4);
+
+
+
+
+
+
+////Updating Employee
+//foreach (Employee employee in list)
+//{
+//    if (employee.Name == "Jack")
+//    {
+//        employee.Salary = 4000;
+//    }
+//}
+//foreach (Employee employee in list)
+//{
+//    Console.WriteLine(employee.Name + "    " + employee.Salary);
+//}
